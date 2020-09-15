eeffc613cc1ff08061d594818683d14456a5335b
ailinnakaganeku <66168554+ailinnakaganeku@users.noreply.github.com>
2020-09-15 11:40:02 -0300

    Update EjercicioEvenCharsOfStr.js

--- EjercicioEvenCharsOfStr.js
+++ EjercicioEvenCharsOfStr.js
@@ -6,14 +6,11 @@
 //"abcdefghijklm" --> ["b", "d", "f", "h", "j", "l"]
 //"a"             --> "invalid string" 
 
-function evenChars(string) {
-    return (string.length < 2 || string.length > 100) ? "invalid string" :
-        [...string].filter((x, i) => i % 2);
-}
+
 function evenChars(string) {
     if (2 > string.length || string.length > 100) return 'invalid string';
     return string.split('').filter(function (item, index) {
         return index % 2 != 0;
     });
 }
-const evenChars = xs => xs.length < 2 || xs.length > 100 ? 'invalid string' : xs.split('').filter((x, i) => i % 2)
+
